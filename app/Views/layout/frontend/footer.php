7a6a5a06de45b236d3b211b41411894eec9ba701
Cat-meoww <pidugunaveensetty@gmail.com>
2023-11-21 10:28:46 +0530

    Header and footer

--- app/Views/layout/frontend/footer.php
+++ app/Views/layout/frontend/footer.php
@@ -1,142 +1,181 @@
 <footer class="footer footer-1">
-    <div class="footer-top">
-        <div class="container">
+        <div class="footer-top">
+          <div class="container">
             <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-3">
-                    <div class="footer-widget widget-contact">
-                        <div class="footer-widget-title">
-                            <h5>quick contact</h5>
-                        </div>
-                        <div class="widget-content">
-                            <p>
-                                If you have any questions or need help, feel free to
-                                contact with our team.
-                            </p>
-                            <ul>
-                                <li class="phone">
-                                    <a href="tel:+91 90039 32323"><i class="fas fa-phone-alt"></i> 90039 32323</a>
-                                </li>
-                                <li class="address">
-                                    <a href="#" style="color:white">45, Pankaja Mills Rd, Sowripalayam Pirivu, Ramanathapuram,
-                                        Coimbatore- 641045</a>
-                                </li>
-                            </ul>
-                            <a class="directions" href="javascript:void(0)"><i class="icon-arrow-right"></i>get directions</a>
-                        </div>
-                    </div>
-                    <!--  End .col-lg-3-->
+              <div class="col-sm-12 col-md-6 col-lg-3">
+                <div class="footer-widget widget-contact">
+                  <div class="footer-widget-title">
+                    <img   src="<?= base_url('frontend/assets/images/favicon/photo1699001614.jpeg') ?>" style="border-radius: 50px;"/>
+                  </div>
+                  <div class="widget-content">
+                    <p style="margin-top: 20px; font-size: 15px;" >
+                      In the Year of 2001, the name GEM Hospital was derived from Gastroenterology and Medical Centre and true to its name. The hospital provides world-class gastro care under one roof.
+                    </p>
+                    
+                   
+                  </div>
                 </div>
-                <div class="col-sm-6 col-md-3 col-lg-2 offset-lg-0">
-                    <div class="footer-widget widget-links">
-                        <div class="footer-widget-title">
-                            <h5>Our Centers</h5>
-                        </div>
-                        <div class="widget-content">
-                            <ul>
-                                <li><a href="#">Coimbatore</a></li>
-                                <li><a href="https://chennai.gemhospitals.com/">Chennai-Perungudi</a></li>
-                                <li>
-                                    <a href="#">Chennai-Aminjikarai </a>
-                                </li>
-                                <li><a href="#">Tiruppur</a></li>
-                                <li><a href="#">Pondicherry</a></li>
-                                <li><a href="#">Erode</a></li>
-                                <li><a href="#">Thrissur</a></li>
+                <!--  End .col-lg-3-->
+              </div>
+              <div class="col-sm-6 col-md-3 col-lg-2 offset-lg-0">
+                <div class="footer-widget widget-links">
+                  <div class="footer-widget-title">
+                    <h5>Our Centers</h5>
+                  </div>
+                  <div class="widget-content">
+                    <ul>
+                      <li><a href="#">Coimbatore</a></li>
+                      <li><a href="https://chennai.gemhospitals.com/">Chennai-Perungudi</a></li>
+                      <li>
+                        <a href="#">Chennai-Aminjikarai </a>
+                      </li>
+                      <li><a href="#">Tiruppur</a></li>
+                      <li><a href="#">Pondicherry</a></li>
+                      <li><a href="#">Erode</a></li>
+                      <li><a href="#">Thrissur</a></li>
+                     
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!--  End .col-lg-2-->
+              <div class="col-sm-6 col-md-3 col-lg-2">
+                <div class="footer-widget widget-links">
+                  <div class="footer-widget-title">
+                    <h5>links</h5>
+                  </div>
+                  <div class="widget-content">
+                    <ul>
+                      <li><a href="aboutus.html">abouts us</a></li>
+                      <li><a href="page-pricing.html">health package</a></li>
+                      <li><a href="insurance.html">insurance</a></li>
+                      <li><a href="page-magazines.html">magazines</a></li>
+                      <li><a href="bmw.html">BMW Report</a></li>
+                      <li><a href="blog.html">blog</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!--  End .col-lg-2-->
+              <div class="col-sm-6 col-md-4 col-lg-2 offset-lg-0">
+                <div class="footer-widget widget-links">
+                  <div class="footer-widget-title">
+                    <h5>help</h5>
+                  </div>
+                  <div class="widget-content">
+                    <ul>
+                      
+                      <li><a href="contact.html">contact</a></li>
+                      <li><a href="career.html">career</a></li>
+                      <li><a href="MGR.html">MGR University</a></li>
+                      <li><a href="NBE.html">NBE</a></li>
+                      <li><a href="press-release.html">Press Release</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <!--  End .col-lg-2-->
+              <div class="col-12 col-sm-6 col-lg-3">
+                <div class="footer-widget widget-newsletter">
+                  <div class="footer-widget-title">
+                    <h5 style="margin-left: 20px;">newsletter</h5>
+                  </div>
+                  <div class="widget-content">
+                    <form class="form-newsletter mailchimp">
+                      <div class="form-group">
+                        <input
+                          class="form-control"
+                          type="email"
+                          name="email"
+                          placeholder="Enter Your Email"
+                          required="required"
+                          style="background-color: white;"
+                          
+                        
 
-                            </ul>
+                        />
+                     
+                          
+                       
+                      </div>
+                      <div class="custom-radio-group">
+                        <div
+                          class="custom-control custom-radio custom-control-inline"
+                        >
+                          <input
+                            class="custom-control-input"
+                            type="radio"
+                            id="customRadioInline2"
+                            name="customRadioInline2"
+                            required="required"
+                          />
+                          <label for="customRadioInline2"
+                            >I Accept The Privacy And Terms</label
+                          >
+                          
                         </div>
-                    </div>
+                        
+                      </div>
+                      <div class="">
+                        <input type="submit" value="submit" class="btn-footer1" />
+                      </div>
+                    </form>
+                    <style>
+                      .btn-footer1{
+                        width: 120px;
+                        height: 50px;
+                        border-radius: 10px;
+                        color: white;
+                        font-weight: bold;
+                        background-color: #118b0f;
+                      }
+                    </style>
+                  </div>
                 </div>
-                <!--  End .col-lg-2-->
-                <div class="col-sm-6 col-md-3 col-lg-2">
-                    <div class="footer-widget widget-links">
-                        <div class="footer-widget-title">
-                            <h5>links</h5>
-                        </div>
-                        <div class="widget-content">
-                            <ul>
-                                <li><a href="aboutus.html">abouts us</a></li>
-                                <li><a href="page-pricing.html">health package</a></li>
-                                <li><a href="insurance.html">insurance</a></li>
-                                <li><a href="page-magazines.html">magazines</a></li>
-                                <li><a href="bmw.html">BMW Report</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!--  End .col-lg-2-->
-                <div class="col-sm-6 col-md-4 col-lg-2 offset-lg-0">
-                    <div class="footer-widget widget-links">
-                        <div class="footer-widget-title">
-                            <h5>help</h5>
-                        </div>
-                        <div class="widget-content">
-                            <ul>
-
-                                <li><a href="contact.html">contact</a></li>
-                                <li><a href="career.html">career</a></li>
-                                <li><a href="MGR.html">MGR University</a></li>
-                                <li><a href="NBE.html">NBE</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!--  End .col-lg-2-->
-                <div class="col-12 col-sm-6 col-lg-3">
-                    <div class="footer-widget widget-newsletter">
-                        <div class="footer-widget-title">
-                            <h5>newsletter</h5>
-                        </div>
-                        <div class="widget-content">
-                            <form class="form-newsletter mailchimp">
-                                <div class="form-group">
-                                    <input class="form-control" type="email" name="email" placeholder="Your Email Address" required="required" />
-                                    <div class="submit">
-                                        <input type="submit" value="" />
-                                    </div>
-                                </div>
-                                <div class="custom-radio-group">
-                                    <div class="custom-control custom-radio custom-control-inline">
-                                        <input class="custom-control-input" type="radio" id="customRadioInline2" name="customRadioInline2" required="required" />
-                                        <label for="customRadioInline2">i accept the privacy and terms</label>
-                                    </div>
-                                </div>
-                                <div class="subscribe-alert"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
             <div class="clearfix"></div>
+          </div>
+          <!--  End .container-->
         </div>
-        <!--  End .container-->
-    </div>
-    <!--  End .footer-center-->
-    <div class="footer-bottom">
-        <div class="container">
+        <!--  End .footer-center-->
+        <div class="footer-bottom">
+          <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <div class="footer-copyright">
-                        <div class="copyright">
-                            <span>&copy; 2023 Gem Hospital by all rights reserved.
-                            </span>
-                            <ul class="list-unstyled social-icons">
-                                <li>
-                                    <a class="share-facebook" href="#"><i class="fab fa-facebook-f"> </i></a>
-                                </li>
-                                <li>
-                                    <a class="share-instagram" href="#"><i class="fab fa-instagram"></i></a>
-                                </li>
-                                <li>
-                                    <a class="share-twitter" href="#"><i class="fab fa-twitter" background-color:skyblue;></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+              <div class="col-12">
+                <div class="footer-copyright">
+                  <div class="copyright">
+                    <span
+                      >&copy; 2023 GEM HOSPITAL by all rights reserved.
+                    </span>
+                    <ul class="list-unstyled social-icons">
+                      <li >
+                        <a class="share-facebook"style="background-color: #3b5998;" href="#"
+                          ><i class="fab fa-facebook-f" > </i
+                        ></a>
+                      </li>
+                      <li>
+                        <a class="share-instagram" href="#" style="background-color: #cd486b;"
+                          ><i class="fab fa-instagram"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a class="share-twitter" href="#" style="background-color: #00acee;"
+                          ><i class="fab fa-twitter"></i
+                        ></a>
+                      </li>
+                      <li>
+                        <a class="share-twitter" href="#" style="background-color:red;"
+                          ><i class="fab fa-youtube"></i
+                        ></a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
+          <!--  End .row-->
         </div>
-        <!--  End .row-->
-    </div>
-    <!--  End .footer-bottom-->
-</footer>
+        <!--  End .footer-bottom-->
+      </footer>
